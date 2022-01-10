2fc3b19435e0b6bb67a4dbdb6c240adec6a160c4
robriks <marsterlund@gmail.com>
2022-01-09 20:03:17 -0500

    initial commit

---
+++ contracts/HuskyCoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// Using OpenZeppelin Contracts v4.4.1 (token/ERC20/ERC20.sol)
+
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Stakeable.sol";
+
+contract HuskyCoin is ERC20, Stakeable {
+    constructor() ERC20("HuskyCoin", "HUSKY") {
+        uint maxSupply;
+        
+    }
+}
---
+++ contracts/Stakeable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+/* 
+ * @notice This Stakeable contract is meant to be inherited by an ERC20 compliant contract to provide staking functionality
+ */
+contract Stakeable {
+    constructor() {}
+}
