e33fee030d6a47affe232c191a14afcd82f5b36e
robriks <marsterlund@gmail.com>
2022-03-21 00:46:08 +0000

    checking that modal functions properly

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "131e32df7c5ae24f"; }
+/******/ 		__webpack_require__.h = function() { return "ee7a618b7160644a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/modal.js
+++ components/modal.js
@@ -1,5 +1,12 @@
+import { useRouter } from "next/router"
 
 export default function Modal() {
+    const router = useRouter
+    
+    function goToWriteup() {
+        router.push('/about')
+    }
+
     return (
         <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -19,14 +26,17 @@ export default function Modal() {
                 <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Mobile Device Detected</h3>
                     <div class="mt-2">
-                    <p class="text-sm text-gray-500">This tutorial is not configured for mobile devices. Please switch to desktop to proceed.</p>
+                    <p class="text-sm text-gray-500">
+                      This tutorial, as well as Web3 at large, is generally not configured for mobile devices due to security and compatibility issues. 
+                      Switching to desktop is required to complete the tutorial. 
+                      You may go read my Web3 writeup page without issue, but keep in mind this site is formatted for desktop and will not be pretty on mobile.
+                    </p>
                     </div>
                 </div>
                 </div>
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Proceed anyway</button>
-                <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+                <button type="button" onClick={goToWriteup} class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Proceed anyway to Web3 writeup</button>
             </div>
             </div>
         </div>
