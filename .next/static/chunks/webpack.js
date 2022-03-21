3a308c761308ce450f404879d94385526406136d
robriks <marsterlund@gmail.com>
2022-03-21 01:49:35 +0000

    forgot curly brace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2e9ae7f9f25872b"; }
+/******/ 		__webpack_require__.h = function() { return "aff2bbe6451211cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
