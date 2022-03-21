f50f6883d991c9cada4b84822b301ec0b3f341c3
robriks <marsterlund@gmail.com>
2022-03-21 00:30:02 +0000

    added modal with mobile detection

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44a337e24e68f8e6"; }
+/******/ 		__webpack_require__.h = function() { return "131e32df7c5ae24f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,26 @@
 import '../styles/globals.css'
-import Link from 'next/link'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import Modal from '../components/modal'
+import { isMobile } from 'react-device-detect'
+
 
 function MyApp({ Component, pageProps }) {
+  if (isMobile) {
+    return (
+      <div>
+        <Modal></Modal>
+      </div>
+    )
+  } else {
   return (
-    <div className='min-h-screen h-full align-content-center'>
-      <Header></Header>
-      <Component {...pageProps} />
-      <Footer></Footer>
-    </div>
-  )
+      <div className='min-h-screen h-auto align-content-center'>
+        <Header></Header>
+        <Component {...pageProps} />
+        <Footer></Footer>
+      </div>
+    )
+  }
 }
 
 export default MyApp
